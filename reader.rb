ff3073b3a99beb36b0956c3183f1265846b953dd
yous <chacha@kaist.ac.kr>
2011-06-23 03:59:25 -0700

    each를 이용해 5개의 URL이 가리키는 파일 내용 출력

--- reader.rb
+++ reader.rb
@@ -1,14 +1,7 @@
 require 'open-uri'
 
 open('http://log.kaist.ac.kr/ESK/ruby-1/urls.txt') {|f|
-  file_url = f.gets
-  open(file_url) {|f2| puts f2.read}
-  file_url = f.gets
-  open(file_url) {|f2| puts f2.read}
-  file_url = f.gets
-  open(file_url) {|f2| puts f2.read}
-  file_url = f.gets
-  open(file_url) {|f2| puts f2.read}
-  file_url = f.gets
-  open(file_url) {|f2| puts f2.read}
+  (0..4).each {
+    file_url = f.gets
+    open(file_url) {|f2| puts f2.read}
 }
