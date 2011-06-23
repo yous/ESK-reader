fdbed5c04ea715a4d6dd3ee1fd5dcd0205d0e1d5
yous <chacha@kaist.ac.kr>
2011-06-23 04:02:24 -0700

    f.gets를 이용해 출력 횟수 결정, 파일 내용 출력

--- reader.rb
+++ reader.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 open('http://log.kaist.ac.kr/ESK/ruby-1/urls.txt') {|f|
-  (0..4).each {
-    file_url = f.gets
+  while file_url = f.gets
     open(file_url) {|f2| puts f2.read}
+  end
 }
